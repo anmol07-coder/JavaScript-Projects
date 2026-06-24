a6a648c71d241b736cc252e44d07d99bb5ab0191
Anmol Tyagi <tyagiji2005@gmail.com>
2026-06-24 22:46:45 +0530

    Updated KeyPress Visualizer

--- KeyPress Visualizer/script.js
+++ KeyPress Visualizer/script.js
@@ -1,11 +1,22 @@
-let key = document.querySelector("#key-box")
+let key = document.querySelector("#key");
+let code = document.querySelector("#code");
+let eventCode = document.querySelector("#eventCode");
+let keyBox = document.querySelector(".key-box");
 
 window.addEventListener("keydown" , function(e){
     if(e.key !== " "){
         key.textContent = e.key;
     }
-   else{
-    key.textContent = "SPC";
-   } 
+
+    else{
+        key.textContent = "SPC";
+    }
+    code.textContent = e.keyCode;
+    eventCode.textContent = e.code;
     
+    keyBox.classList.add("animate");
+
+    setTimeout(function(){
+        keyBox.classList.remove("animate");
+    },300);
 })
