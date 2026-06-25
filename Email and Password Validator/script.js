b408450fce05cd30c962b5836a1a4be8b29244ea
Anmol Tyagi <tyagiji2005@gmail.com>
2026-06-25 16:22:13 +0530

    Updated Email and Pasword Validator

--- Email and Password Validator/script.js
+++ Email and Password Validator/script.js
@@ -1,42 +1,127 @@
+let form = document.querySelector("#validator-form");
 let email = document.querySelector("#email");
 let password = document.querySelector("#password");
+let errorEmail = document.querySelector("#email-message");
+let errorPassword = document.querySelector("#password-message");
+let toggleBtn = document.querySelector("#toggle-password");
+let strengthBar = document.querySelector("#strength-fill");
 let validate = document.querySelector("#submit-btn");
-let successMsg = document.querySelector("#success-message");
-let emailError = document.querySelector("#email-error");
-let passError = document.querySelector("#password-error")
+let outerMostDiv = document.querySelector(".container");
 
-const emailRegexEqn = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const passRegexEqn = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
-
-validate.addEventListener("click" , function(e){
-
-    emailError.textContent = "";
-    passError.textContent = "";
-    successMsg.textContent = "";
+form.addEventListener("submit" , function(e){
 
     e.preventDefault();
 
-    let isEmailValid = emailRegexEqn.test(email.value);
-    let isPassValid = passRegexEqn.test(password.value);
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@(gmail|yahoo|outlook|hotmail)\.(com|in|org|net)$/;
+    const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    let isEmailValid =  emailRegex.test(email.value.trim());
+    let isPassValid = passRegex.test(password.value.trim());
 
     if(!isEmailValid){
-        emailError.textContent = "Email not valid";
-        emailError.style.color = "red";
+        errorEmail.textContent = "Invalid Email";
+        errorEmail.style.color = "red";
+
+        setTimeout(function(){
+            errorEmail.textContent = "";
+        },3000);
+
         email.value = "";
     }
 
     if(!isPassValid){
-        passError.textContent = "Password not valid";
-        passError.style.color = "red";
+        errorPassword.textContent = "Invalid Password";
+        errorPassword.style.color = "red";
+
+        setTimeout(function(){
+            errorPassword.textContent = "";
+        },3000);
+
         password.value = "";
     }
 
     if(isEmailValid && isPassValid){
-        successMsg.textContent = "VALIDATED";
-        successMsg.style.color = "green";
+
+        let successMsg = document.querySelector(".success");
+        if(!successMsg){
+            successMsg = document.createElement("div");
+            successMsg.style.color = "green";
+            outerMostDiv.appendChild(successMsg);
+        }
+
+        successMsg.textContent = "Password and Email validated";
+        successMsg.classList.add("success");
+
         email.value = "";
         password.value = "";
+
+        setTimeout(function(){
+            successMsg.textContent = "";
+        },3000);
     }
 
     
+
+})
+
+toggleBtn.addEventListener("click" , function(){
+    if(password.getAttribute("type") === "password"){
+        password.removeAttribute("type");
+        password.setAttribute("type" , "text");
+    }
+
+    else{
+        password.removeAttribute("type");
+        password.setAttribute("type" , "password");
+    }
+    
+})
+
+password.addEventListener("input" , function(){
+    let pass = password.value.trim();
+
+    if(pass.length === 0){
+        strengthBar.style.width = "0%";
+        return;
+    }
+
+    let hasLowercase = /[a-z]/.test(pass);
+    let hasUppercase = /[A-Z]/.test(pass);
+    let hasNumber = /\d/.test(pass);
+    let hasSpecial = /[@$!%*?&]/.test(pass);
+
+    let passPoints = 0;
+    if(password.value.length >= 8){
+        passPoints++;
+    }
+
+    if(hasLowercase){
+        passPoints++;
+    }
+
+    if(hasNumber){
+        passPoints++;
+    }
+
+    if(hasSpecial){
+        passPoints++;
+    }
+
+    if(hasUppercase){
+        passPoints++;
+    }
+
+    if(passPoints <= 2){
+        strengthBar.style.width = "30%";
+        strengthBar.style.background = "red";
+    }
+    else if(passPoints <= 4){
+        strengthBar.style.width = "60%";
+        strengthBar.style.background = "orange";
+    }
+    else{
+        strengthBar.style.width = "100%";
+        strengthBar.style.background = "green";
+    }
+
 })
